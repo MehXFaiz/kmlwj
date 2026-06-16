d3f32ec17c97f32596235477f288c46563db0ce2
Dania Shabih <faizan2311e@aptechsite.net>
2026-06-16 13:54:42 +0500

    .

--- src/services/beneficiaryService.js
+++ src/services/beneficiaryService.js
@@ -1,8 +1,8 @@
 import api from './api';
 
 export const beneficiaryService = {
-  getAll: () => api.get('/v1/beneficiaries').then(res => res.data),
-  create: (data) => api.post('/v1/beneficiaries', data).then(res => res.data),
-  update: (id, data) => api.put(`/v1/beneficiaries?id=${id}`, data).then(res => res.data),
-  delete: (id) => api.delete(`/v1/beneficiaries?id=${id}`).then(res => res.data),
+  getAll: () => api.get('/api/v1/beneficiaries').then(res => res.data),
+  create: (data) => api.post('/api/v1/beneficiaries', data).then(res => res.data),
+  update: (id, data) => api.put(`/api/v1/beneficiaries?id=${id}`, data).then(res => res.data),
+  delete: (id) => api.delete(`/api/v1/beneficiaries?id=${id}`).then(res => res.data),
 };
--- src/services/donationService.js
+++ src/services/donationService.js
@@ -1,8 +1,8 @@
 import api from './api';
 
 export const donationService = {
-  getAll: () => api.get('/v1/donations').then(res => res.data),
-  create: (data) => api.post('/v1/donations', data).then(res => res.data),
-  update: (id, data) => api.put(`/v1/donations?id=${id}`, data).then(res => res.data),
-  approve: (id) => api.post(`/v1/donations?action=approve`, { id }).then(res => res.data),
+  getAll: () => api.get('/api/v1/donations').then(res => res.data),
+  create: (data) => api.post('/api/v1/donations', data).then(res => res.data),
+  update: (id, data) => api.put(`/api/v1/donations?id=${id}`, data).then(res => res.data),
+  approve: (id) => api.post(`/api/v1/donations?action=approve`, { id }).then(res => res.data),
 };
