9afb8486199f8aa4777952bf716d02f9671ce90d
faizanjumani <faizan2311e@aptechsite.net>
2026-07-12 13:03:55 -0700

    .

--- src/components/common/VoucherSlipModal.jsx
+++ src/components/common/VoucherSlipModal.jsx
@@ -143,11 +143,6 @@ export const VoucherSlipModal = ({
               ))}
             </div>
 
-            {/* Background Emblem/Watermark */}
-            <div className="absolute inset-0 flex items-center justify-center pointer-events-none select-none overflow-hidden z-0 opacity-[0.08] print:opacity-[0.10]">
-              <img src={logoImg} alt="KMLWJ Logo Watermark" className="w-[440px] h-[440px] sm:w-[560px] sm:h-[560px] object-contain" />
-            </div>
-
             {/* Main Content Area */}
             <div className="flex-1 relative z-10 flex flex-col justify-between">
               
@@ -230,8 +225,12 @@ export const VoucherSlipModal = ({
               </div>
 
               {/* Main Particulars Table */}
-              <div className="border-2 border-slate-800 my-2 bg-white/75 print:bg-transparent rounded overflow-hidden">
-                <table className="w-full border-collapse text-xs sm:text-sm">
+              <div className="relative border-2 border-slate-800 my-2 bg-white/75 print:bg-transparent rounded overflow-hidden">
+                {/* Fixed Watermark Inside Particulars Table */}
+                <div className="absolute inset-0 flex items-center justify-center pointer-events-none select-none overflow-hidden z-0 opacity-[0.10] print:opacity-[0.12]">
+                  <img src={logoImg} alt="KMLWJ Logo Watermark" className="w-40 h-40 sm:w-48 sm:h-48 object-contain" />
+                </div>
+                <table className="relative z-10 w-full border-collapse text-xs sm:text-sm">
                   <thead>
                     <tr className="border-b-2 border-slate-800 bg-slate-100 font-bold text-slate-900">
                       <th className="py-2 px-3 text-left w-3/4 border-r-2 border-slate-800 uppercase tracking-wider">
